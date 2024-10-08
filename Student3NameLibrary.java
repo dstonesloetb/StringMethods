e3932a342da1a3735f36b1fa9974da0bfb8d7eb6
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-10-08 16:59:46 +0100

    Update Student3NameLibrary.java

--- Student3NameLibrary.java
+++ Student3NameLibrary.java
@@ -1,1 +1,19 @@
+//Insert relevant package name
 
+//Rename class using Student's Name
+public class Student3NameLibrary {
+
+    //Insert student 3's function's below
+
+   //Example function 
+    public static int getLength(String str) {
+        return str.length();
+    }
+
+   //Function 2
+
+
+  //Function 3 (if applicable)
+   
+    
+} //end class 
