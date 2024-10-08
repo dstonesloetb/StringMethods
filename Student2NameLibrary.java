453ef079d30c8424564e700cae7b960dd43da315
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-10-08 16:58:49 +0100

    Create Student2NameLibrary.java

---
+++ Student2NameLibrary.java
@@ -0,0 +1,19 @@
+//Insert relevant package name
+
+//Rename class using Student's Name
+public class Student2NameLibrary {
+
+    //Insert student 2's function's below
+
+   //Example function 
+    public static int getLength(String str) {
+        return str.length();
+    }
+
+   //Function 2
+
+
+  //Function 3 (if applicable)
+   
+    
+} //end class 
